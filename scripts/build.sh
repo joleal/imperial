da36adbc82b2665c9ccc7cbe25fe8525e94f388b
joao <joao.leal@gmail.com>
2020-06-17 16:09:47 +0100

    fixed presentation folder\

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,6 @@
 ##build Go API
+go get -d /home/ubuntu/imperial/api all
 go build /home/ubuntu/imperial/api/main.go
 
 ##build Vue app
-npm run build npm run build --prefix /home/ubuntu/imperial/imperial/
+npm run build npm run build --prefix /home/ubuntu/imperial/presentation/
